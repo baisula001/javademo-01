62df46b32cfda01f7a1392563a23be42ddf9c713
3575205331a@gmail.com <3575205331a@gmail.com>
2023-03-20 00:25:30 +0100

    Initial commit

---
+++ first-demo.java
@@ -0,0 +1,10 @@
+class HelloChina{
+
+    public static void main(String[] args){
+
+        System.out.println("hello world,");
+		System.out.println("hello world,");
+		System.out.println("hello world,");
+    }
+
+}
---
+++ first-demo01.java
@@ -0,0 +1,10 @@
+class HelloChina{
+
+    public static void main(String[] args){
+		// this is single line comment;
+        System.out.println("hello world,");
+		System.out.println("hello world,");
+		System.out.println("hello world,");
+    }
+
+}
